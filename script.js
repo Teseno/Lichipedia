f9d96e133d810f6b9c46b45bcd16453a07b496c4
Alex <ajnormandeau28@gmail.com>
2024-05-23 14:04:42 -0400

    Moved to pure JS

---
+++ script.js
@@ -0,0 +1,24 @@
+async function api() {
+    let section = radioGetter();
+    let output = document.getElementById("thing");
+    if (section != "") {
+        let link = "https://www.dnd5eapi.co/api/" + section;
+        let user = document.getElementById("user").value;
+
+        const response = await fetch (link);
+        const hold = await response.json();
+        console.log(hold.results[0].name);
+        output.innerHTML = hold.results[0].name;
+    }else{
+        output.innerHTML = "Not Found";
+    }
+}
+function radioGetter() {
+    var checker = document.querySelector('input[name="a"]:checked');
+    if (checker != null) {
+        return checker.id;
+    }
+    else {
+        return "";
+    }
+}
